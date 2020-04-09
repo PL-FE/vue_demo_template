ecdc944f81b874fb92f981810caafd604c8b6709
pengliang <624151713@qq.com>
2020-04-09 23:48:42 +0800

    优化eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,14 @@ module.exports = {
     env: {},
     globals: {},
     extends: [
-        "plugin:vue/essential",
-        '@vue/standard'
+        // 'plugin:vue/essential'
+        //  '@vue/standard'
     ],
+    parser: 'vue-eslint-parser',
     rules: {
-        'no-console': "off",
+        'no-console': 'off',
         'no-debugger': process.env.NODE.ENV === 'production' ? 'error' : 'warn',
-        'camelcase': "off"
+        camelcase: 'off'
     },
     parserOptions: {
         parser: 'babel-eslint'
