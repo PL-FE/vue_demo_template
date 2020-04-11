97fc94ebf4437b056db0c04596b07a928fda5d41
pengliang <624151713@qq.com>
2020-04-11 16:18:48 +0800

    update, add scollView

--- src/views/index.js
+++ src/views/index.js
@@ -1,12 +1,12 @@
-const requireComponents = require.context("./main-page/", false, /\.vue/);
+const requireComponents = require.context('./main-page/', false, /\.vue/)
 
-export default function install(Vue) {
-    requireComponents.keys().forEach(fileName => {
-        // 组件实例
-        const reqCom = requireComponents(fileName);
-        // 截取路径作为组件名
-        let reqComName = fileName.replace(/\.\//, '').replace(/\.vue/, '');
-        // 组件挂载
-        Vue.component(reqComName, reqCom.default || reqCom);
-    });
+export default function install (Vue) {
+  requireComponents.keys().forEach(fileName => {
+    // 组件实例
+    const reqCom = requireComponents(fileName)
+    // 截取路径作为组件名
+    const reqComName = fileName.replace(/\.\//, '').replace(/\.vue/, '')
+    // 组件挂载
+    Vue.component(reqComName, reqCom.default || reqCom)
+  })
 }
--- src/views/main-page/TestScollView.vue
+++ src/views/main-page/TestScollView.vue
@@ -1,22 +1,30 @@
 <template>
   <div class="container">
     ScollView
-    <ScollView></ScollView>
+    <ScollView width="60%">
+      <div class="d"
+           v-for="i in 10"
+           :key="i"></div>
+    </ScollView>
+    <span>这是右边行内元素</span>
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {
-
-    }
+    return {}
   },
-  mounted () {
-
-  }
+  mounted () { }
 }
 </script>
 
 <style scoped lang="less">
+.d {
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  background: red;
+  margin: 0 10px;
+}
 </style>
