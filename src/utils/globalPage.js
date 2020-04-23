2437a20890552d13fa10954719796de0575cde55
pengliang <624151713@qq.com>
2020-04-23 23:30:04 +0800

    update_init

---
+++ src/utils/globalPage.js
@@ -0,0 +1,12 @@
+const requireComponents = require.context('../views/', false, /\.vue/)
+
+export default function install (Vue) {
+  requireComponents.keys().forEach(fileName => {
+    // 组件实例
+    const reqCom = requireComponents(fileName)
+    // 截取路径作为组件名
+    const reqComName = fileName.replace(/\.\//, '').replace(/\.vue/, '')
+    // 组件挂载
+    Vue.component(reqComName, reqCom.default || reqCom)
+  })
+}
---
+++ src/views/Test1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    Test1ss
+    <Component1></Component1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
